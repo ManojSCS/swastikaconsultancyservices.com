82b365b01220f60c75adfc23cfdd60c712902b98
ManojSCS <manoj@swastikaconsultancyservices.com>
2026-06-26 22:24:50 +0530

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+// Replace these placeholders with your Firebase project config
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+if (window.firebase && !firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
